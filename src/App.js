523f0478f282f92e066ee0ae9c0fea6799c31f63
meriam <hamdaoui.meriam85@gmail.com>
2022-04-01 17:06:33 +0200

    little bit styling

--- src/App.js
+++ src/App.js
@@ -8,33 +8,44 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      btnText : "show profile",
       show : true,
-      count:0
+      count:0,
+      //set text for button
+      btnText : "show profile",
+      //set color for btn
+      color : "primary",      
     };
   }
-  // component did mount function
+  
+  // component did mount and setInterval function
+  // wich show the lifecycle of the component
   componentDidMount(){
     setInterval(() => this.setState({count: this.state.count+1}),1000);
   }
-  // onclick function
+  // onclick function will change the texte and color of button 
+  //beside showing and hiding profile
   btnClick(){
     this.setState({show: !this.state.show})
     if(this.state.btnText === "show profile"){
-      this.setState({btnText : "hide profile"})
+      this.setState({
+        btnText : "hide profile",
+        color: "danger"})
     }
     if(this.state.btnText === "hide profile"){
-      this.setState({btnText : "show profile"})
+      this.setState({
+        btnText : "show profile",
+        color:"success"})
     }
   }
-
+  
   render() {
     return (
       <div className='App'>       
         {!this.state.show && <Person />}
         <h2> {this.state.count}</h2>
         <Button className='button-53'
-              onClick={() => this.btnClick()}>
+              onClick={() => this.btnClick()}
+              variant={this.state.color}>
           {this.state.btnText}
         </Button>
       </div>
